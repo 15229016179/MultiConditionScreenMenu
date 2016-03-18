1a2bcb90963a3479a692533a7cfa2b4a2512c768
我是小明 <sgyingyin@sina.com>
2016-03-18 11:14:32 +0800

    可动态添加筛选项的多条件筛选菜单栏
    可动态添加筛选项的多条件筛选菜单栏,可添加多个筛选项,多级筛选列表

---
+++ src/com/xiaoming/dropdownmenu/entity/City.java
@@ -0,0 +1,15 @@
+package com.xiaoming.dropdownmenu.entity;
+
+public class City {
+
+	private String cityName;
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+}
